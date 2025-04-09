16c3494cf5215b21b5829a258845fe8823c8af7d
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-08 21:18:10 -0300

    Navbar Menu full implementation

--- src/app/components/header/navbar/navbar.component.ts
+++ src/app/components/header/navbar/navbar.component.ts
@@ -1,14 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
   isEnglish = false;
+  showToast = false;
 
   constructor(private translate: TranslateService) {
     this.isEnglish = this.translate.currentLang === 'en-US';
@@ -18,6 +21,28 @@ export class NavbarComponent {
     this.isEnglish = !this.isEnglish;
     const newLang = this.isEnglish ? 'en-US' : 'pt-BR';
     this.translate.use(newLang);
+  }
+
+  copyEmail() {
+    const email = "pedrovitormlopes@gmail.com";
+    navigator.clipboard.writeText(email)
+    .then(() => {
+      this.showToast = true;
+      setTimeout(() => {
+        this.showToast = false;
+      }, 3000);
+    })
+  }
+
+  copyPhoneNumber() {
+    const phoneNumber = "+55 11 952304438";
+    navigator.clipboard.writeText(phoneNumber)
+    .then(() => {
+      this.showToast = true;
+      setTimeout(() => {
+        this.showToast = false;
+      }, 3000);
+    })
   }
 }
 
