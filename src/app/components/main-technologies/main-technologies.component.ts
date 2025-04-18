063085f76c3dca371501b48b86f3c79cdfebe435
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-18 18:23:34 -0300

    Main technologies done

--- src/app/components/main-technologies/main-technologies.component.ts
+++ src/app/components/main-technologies/main-technologies.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFigma } from '@fortawesome/free-brands-svg-icons';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faFigma, faReact, faGit, faNode } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe, faCode, faWater, faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main-technologies',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, TranslateModule],
   templateUrl: './main-technologies.component.html',
   styleUrl: './main-technologies.component.scss'
 })
 export class MainTechnologiesComponent {
   faGlobe = faGlobe;
   faFigma = faFigma;
+  faCode = faCode;
+  faReact = faReact;
+  faWater = faWater;
+  faGit = faGit;
+  faDatabase = faDatabase;
+  faNode = faNode;
 
 }
