69fd6258d5ec8bdf9d5a74e905299c03785d75fe
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-18 17:24:46 -0300

    Navbar Changes

--- src/app/components/main-technologies/main-technologies.component.ts
+++ src/app/components/main-technologies/main-technologies.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFigma } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-technologies',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './main-technologies.component.html',
   styleUrl: './main-technologies.component.scss'
 })
 export class MainTechnologiesComponent {
+  faGlobe = faGlobe;
+  faFigma = faFigma;
 
 }
