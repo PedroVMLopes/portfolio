8a435848932da3f6c3af64e555bac8f624388b15
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-28 18:12:40 -0300

    Minor changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [TranslateModule],
+  imports: [TranslateModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
 }
