03be16254f63af1831849c6e9bafda1fb6d01afe
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-29 00:45:34 -0300

    Description Boxes add

--- src/app/components/projects/tabletop/description-boxes/description-boxes.component.ts
+++ src/app/components/projects/tabletop/description-boxes/description-boxes.component.ts
@@ -1,12 +1,17 @@
+import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-description-boxes',
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgSwitch, NgSwitchCase],
   templateUrl: './description-boxes.component.html',
   styleUrl: './description-boxes.component.scss'
 })
 export class DescriptionBoxesComponent {
+  currentSlideOfBox1 = 0;
 
+  selectSlide(index: number) {
+    this.currentSlideOfBox1 = index;
+  }
 }
