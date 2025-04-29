a3e7ee0b37dd7029171e41ecb633704d5d0a3875
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-29 11:44:44 -0300

    Functionalities changes

--- src/app/components/projects/tabletop/description-boxes/description-boxes.component.ts
+++ src/app/components/projects/tabletop/description-boxes/description-boxes.component.ts
@@ -1,15 +1,20 @@
 import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFeather, faGears } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-description-boxes',
-  imports: [TranslateModule, NgSwitch, NgSwitchCase],
+  imports: [TranslateModule, NgSwitch, NgSwitchCase, FontAwesomeModule],
   templateUrl: './description-boxes.component.html',
   styleUrl: './description-boxes.component.scss'
 })
 export class DescriptionBoxesComponent {
   currentSlideOfBox1 = 0;
+
+  faFeather = faFeather;
+  faGears = faGears;
 
   selectSlide(index: number) {
     this.currentSlideOfBox1 = index;
