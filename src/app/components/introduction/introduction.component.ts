0fede4d34be2cd0d7a2172b82c128c617915adb0
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-04-29 21:37:34 -0300

    Refinement preparing to the deploy

--- src/app/components/introduction/introduction.component.ts
+++ src/app/components/introduction/introduction.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FontAwesomeModule],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss'
 })
 export class IntroductionComponent {
+  faAngleDown = faAngleDown;
 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
